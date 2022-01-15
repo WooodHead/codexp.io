795ffc248242d7fad7a9668230b1613ac0d38fe9
WooodHead <jcwsw129@gmail.com>
2022-01-15 19:11:07 +0800

    chore: Update

--- next.config.js
+++ next.config.js
@@ -1,35 +1,20 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 
-const env = require('dotenv').config().parsed || {};
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
 
-const withPlugins = require('next-compose-plugins');
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const plugins = [];
-
-if (process.env.ANALYZE === 'true') {
-  plugins.push([require('@next/bundle-analyzer'), { enabled: true }]);
-}
-const configs = withPlugins(plugins, {
-  // swcMinify: true,
-  // experimental: {
-  //   // ssr and displayName are configured by default
-  //   styledComponents: !isProd,
-  // },
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, options) => {
     config.plugins.push(new WindiCSSWebpackPlugin());
 
     return config;
   },
-  env: {
-    PORT: 3333,
-    ...env,
-  },
-  assetPrefix: isProd ? env.ASSETPREFIX || undefined : undefined,
-  eslint: {
-    ignoreDuringBuilds: true,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
   },
 });
-
-module.exports = configs;
