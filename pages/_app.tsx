6e7d28e385316a9b7672c8c324e3014fcee5f2c3
WooodHead <jcwsw129@gmail.com>
2022-01-15 19:29:46 +0800

    chore: Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,19 @@ import './global.css';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 
-// import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs';
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // Sentry.init({
-    //   dsn: 'https://5b565ccc507f4a0d8d213f24c951cbbc@o144425.ingest.sentry.io/6150867',
+    Sentry.init({
+      dsn: 'https://5b565ccc507f4a0d8d213f24c951cbbc@o144425.ingest.sentry.io/6150867',
 
-    //   // Set tracesSampleRate to 1.0 to capture 100%
-    //   // of transactions for performance monitoring.
-    //   // We recommend adjusting this value in production
-    //   tracesSampleRate: 1.0,
-    // });
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+    });
   }, []);
   return (
     <>
